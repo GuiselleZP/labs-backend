f88f5a9cd23182be403edb41df1dfc8a6f29a6a2
GuiselleZP <guiselle2097@gmail.com>
2020-06-10 21:59:36 -0500

    SuccessTest.java v01

---
+++ src/test/java/co/edu/unal/software_engineering/labs/service/SuccessTest.java
@@ -0,0 +1,46 @@
+package co.edu.unal.software_engineering.labs.service;
+
+import co.edu.unal.software_engineering.labs.model.Course;
+import co.edu.unal.software_engineering.labs.pojo.CoursePOJO;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.InjectMocks;
+import org.mockito.junit.MockitoJUnitRunner;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertNotNull;
+
+@RunWith( MockitoJUnitRunner.class )
+public class SuccessTest{
+
+    @InjectMocks
+    private CourseService courseService;
+
+
+    public SuccessTest( ){ }
+
+
+    @Test
+    public void mapperCourseEntityTest( ){
+        String courseName = "Sad course :c";
+        Integer durationHours = 3;
+        Integer verifyDurationHours = 0;
+
+        CoursePOJO pojo = new CoursePOJO( );
+        pojo.setCourseName( courseName );
+        pojo.setDurationHours( durationHours);
+        Course course = courseService.mapperCourseEntity( pojo );
+
+        assertNotNull( course );
+        assertNotNull( course.getCourseName( ) );
+        assertNotNull( course.getDurationHours( ) );
+
+        verifyDurationHours = course.getDurationHours();
+
+        assertEquals( verifyDurationHours, pojo.getCourseName( ) );
+        assertEquals( course.getCourseName( ), pojo.getCourseName( ) );
+        assertEquals( course.getDurationHours( ), pojo.getDurationHours( ) );
+
+    }
+
+}
